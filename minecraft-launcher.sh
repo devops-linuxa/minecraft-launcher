ba17b952011254ecc6a7735adb8547620398658b
Archi <archi@linuxa.ru>
2026-05-29 00:49:00 +0300

    Added: func is_version_installed and var JAVA

--- minecraft-launcher.sh
+++ minecraft-launcher.sh
@@ -3,6 +3,8 @@
 set -e
 
 MINECRAFT_VERSION=${1:-"1.21.1"}
+JAVA_VERSION='26'
+JAVA="/usr/lib/jvm/java-${JAVA_VERSION}-openjdk/bin/java"
 MINECRAFT_DIR="${HOME}/.minecraft"
 MOJANG_MANIFEST_JSON_URL='https://piston-meta.mojang.com/mc/game/version_manifest_v2.json'
 THREADS=$(nproc)
@@ -28,10 +30,16 @@ UUID_MAKENA="00000000-0000-0000-0000-000000000004"  # Девка Тёмная
 UUID=$UUID_SUNNY
 
 cd ${MINECRAFT_DIR}
+
+is_version_installed(){
+    ls -1 versions/ |\
+        grep -x $MINECRAFT_VERSION >/dev/null \
+        && true || false 
+}
+
 mkdir -p ${MINECRAFT_DIR}/versions/${MINECRAFT_VERSION}
 mkdir -p ${MINECRAFT_DIR}/assets/indexes
 
-### START:
 echo 'Получаем главный Манифест MOJANG:'
 MOJANG_MANIFEST_JSON=$(
     curl -s $MOJANG_MANIFEST_JSON_URL | jq
@@ -52,7 +60,7 @@ if [[ ! ${minecraft_version_json_data} ]];then
 fi
 echo 'OK'
 
-if [[ ! -f $JSON_FILE ]];then
+if ! is_version_installed;then
     echo "Получаем ссылку для json файла:"
     json_manifest_url=$(
         echo "$minecraft_version_json_data" | jq -r .url
@@ -71,9 +79,7 @@ if [[ ! -f $JSON_FILE ]];then
         exit 1
     fi
     echo 'OK'
-fi
 
-if [[ ! -f $JAR_FILE ]];then
     echo "Получаем ссылку для jar:"
     jar_client_url=$(
         cat ${JSON_FILE} |\
@@ -92,37 +98,35 @@ if [[ ! -f $JAR_FILE ]];then
         exit 1
     fi
     echo 'OK'
-fi
 
-echo 'Вытаскиваем список библиотек:'
-libs_list=$(
-    jq '.libraries[].downloads.artifact | select(.path | contains("linux") or (contains("natives") | not)) | .path' ${JSON_FILE}
-)
-if [[ ! $libs_list ]];then
-    echo 'FAILED'
-    exit 1
-fi
-echo 'OK'
-
-
-echo 'Качаем все библиотеки и генерируем нужные директории:'
-cd ${MINECRAFT_DIR}/
-if ! jq -r '.libraries[].downloads.artifact | select(.path | contains("linux") or (contains("natives") | not)) | "\(.url)\n\(.path)"' \
-        "${JSON_FILE}" |\
-        xargs -P "$THREADS" -n 2 bash -c '
-    url="$1"
-    path="libraries/$2"
-    if [ -n "$url" ] && [ ! -f "$path" ]; then
-        mkdir -p "$(dirname "$path")"
-        curl -fL --progress-bar -o "$path" "$url"
+    echo 'Вытаскиваем список библиотек:'
+    libs_list=$(
+        jq '.libraries[].downloads.artifact | select(.path | contains("linux") or (contains("natives") | not)) | .path' ${JSON_FILE}
+    )
+    if [[ ! $libs_list ]];then
+        echo 'FAILED'
+        exit 1
     fi
-' _ ;
-then
-    echo 'FAILED'
-    exit 1
-fi
-echo 'OK'
+    echo 'OK'
 
+    echo 'Качаем все библиотеки и генерируем нужные директории:'
+    cd ${MINECRAFT_DIR}/
+    if ! jq -r '.libraries[].downloads.artifact | select(.path | contains("linux") or (contains("natives") | not)) | "\(.url)\n\(.path)"' \
+            "${JSON_FILE}" |\
+            xargs -P "$THREADS" -n 2 bash -c '
+        url="$1"
+        path="libraries/$2"
+        if [ -n "$url" ] && [ ! -f "$path" ]; then
+            mkdir -p "$(dirname "$path")"
+            curl -fL --progress-bar -o "$path" "$url"
+        fi
+    ' _ ;
+    then
+        echo 'FAILED'
+        exit 1
+    fi
+    echo 'OK'
+fi
 
 echo "Получаем ссылку на assetIndex:"
 asset_index_url=$(
@@ -144,7 +148,6 @@ if ! curl -fL --progress-bar -o assets/indexes/$asset_index_file $asset_index_ur
     exit 1
 fi
 echo 'OK'
-
 
 echo 'Выкачиваем все ассеты (самый долгий пункт):'
 if ! jq -r '.objects[].hash' assets/indexes/$(basename $asset_index_url) |\
@@ -183,7 +186,7 @@ CLASSPATH="${CLASSPATH}${CLIENT_JAR}"
 
 
 # Запуск игры
-java -Xmx4G -XX:+UseG1GC \
+${JAVA} -Xmx4G -XX:+UseG1GC \
     -Djava.library.path="$MINECRAFT_DIR/versions/$VERSION/natives" \
     -cp "$CLASSPATH" \
     net.minecraft.client.main.Main \
